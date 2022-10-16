ed8e69e0b272f1e1372cff291226722ced89bd36
Andrew Nguyen <anguyen1281@student.cccd.edu>
2022-10-16 04:30:03 -0700

    add attack player scripts

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.transform.position + new Vector3(0, 1, -5);
+    }
+}
