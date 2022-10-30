5e73351b6e614eca05866332f2be8d19dbcdf237
JH159753 <JHL159753@gmail.com>
2022-10-30 03:48:31 -0700

    changed EnemySpawner.cs
    Enemies will now spawn specifically on tiles

--- Assets/scripts/EnemySpawner.cs
+++ Assets/scripts/EnemySpawner.cs
@@ -19,7 +19,7 @@ public class EnemySpawner : MonoBehaviour
     private IEnumerator spawnEnemy(float interval, GameObject enemy)
     {
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Mathf.Round(Random.Range(-5f, 5f)), Mathf.Round(Random.Range(-6f, 6f)), 0), Quaternion.identity);
         StartCoroutine(spawnEnemy(interval, enemy));
     }
 }
