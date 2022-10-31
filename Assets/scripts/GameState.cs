d85c62ac074ad2a9c8c88816d82d275694d0a56d
JH159753 <JHL159753@gmail.com>
2022-10-31 02:57:16 -0700

    attempting to figure out gamestate

--- Assets/scripts/GameState.cs
+++ Assets/scripts/GameState.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 public class GameState
 {
-
-    public static int SpeedFactor = 10;
     public static bool PlayerTurn = true;
+    public int turnCount = 1;
 
-    
+    //This gets called once at start, but there shouldn't be much to do here?
+    void Start()
+    {
+
+    }
+
+    //This gets called every frame
+    void Update()
+    {
+
+    }
+
+    Debug.Log("Currently player turn");
+    WaitWhile(PlayerTurn == true);
+
+    //if the player moves or attacks, set PlayerTurn to false
+
+    turnCount++;
+
+    Debug.Log("Currently enemy turn");
+    WaitUntil(PlayerTurn == false);
+
+    //let all enemies do their action, then set PlayerTurn to true
+    //presumably this would be a for loop that goes through each enemy
+
+    turnCount++;
 }
