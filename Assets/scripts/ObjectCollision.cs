7c1d03c68b0d8288fb758111feb290b514cda310
Andrew Nguyen <anguyen1281@student.cccd.edu>
2022-11-04 21:35:34 -0700

    "enemy collider" and "spawn" WIP

---
+++ Assets/scripts/ObjectCollision.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ObjectCollision : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.AddComponent<Rigidbody2D>() as Rigidbody2D;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        Debug.Log("hit detected");
+        rb.velocity = new Vector2(0.0f, 0f);
+    }
+}
