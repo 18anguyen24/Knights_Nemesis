25fc05120fffb649e3b17d3360e14dc746ee8a23
Ratske99 <sethlaske9999@gmail.com>
2022-11-21 13:14:02 -0800

    UI Edits and Enemy Spawner additions

--- Assets/scripts/EnemySpawner.cs
+++ Assets/scripts/EnemySpawner.cs
@@ -5,24 +5,33 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
 
-    [SerializeField]
-    private GameObject enemyPrefab;
+    //Cumulative chance, each enemy type should be increasingly more likely
+    public GameObject enemyType1;
+    public int type1SpawnRate;
+    public GameObject enemyType2;
+    public int type2SpawnRate;
+    public GameObject enemyType3;
+    public int type3SpawnRate;
+    public GameObject enemyType4;
+    public int type4SpawnRate;
+    public GameObject enemyType5;
+    //private int type5SpawnRate = 100;
 
     public LayerMask WhatStopsMovement;
 
     public int maxEnemies;
     public int chanceToSpawn;
 
-    //Specify width and height of the spawner, starting from the origin at the bottom left corner. Can just place a spawner per room, probably
+    //Specify width and height of the spawner, starting from the origin at the bottom left corner. 
 
     public int width = 5;
     public int height = 7;
 
     void Start()
     {
-        //newEnemy();
-        //newEnemy();
-        //newEnemy();
+        type2SpawnRate += type1SpawnRate;
+        type3SpawnRate += type2SpawnRate;
+        type4SpawnRate += type3SpawnRate;
     }
 
 
@@ -35,8 +44,28 @@ public class EnemySpawner : MonoBehaviour
 
         if(!Physics2D.OverlapCircle(enemyDrop, .2f, WhatStopsMovement) && Random.Range(0, 100) < chanceToSpawn && GameState.Enemies.Count < maxEnemies)
         {
+            int enemyType = (Random.Range(0, 99));
+            Debug.Log("Odd: " + enemyType);
+            if (enemyType < type1SpawnRate) {
+                GameObject newEnemy = Instantiate(enemyType1, enemyDrop, Quaternion.identity);
+            } else if (enemyType < type2SpawnRate)
+            {
+                GameObject newEnemy = Instantiate(enemyType2, enemyDrop, Quaternion.identity);
+            }
+            else if (enemyType < type3SpawnRate)
+            {
+                GameObject newEnemy = Instantiate(enemyType3, enemyDrop, Quaternion.identity);
+            }
+            else if (enemyType < type4SpawnRate)
+            {
+                GameObject newEnemy = Instantiate(enemyType4, enemyDrop, Quaternion.identity);
+            }
+            else 
+            {
+                GameObject newEnemy = Instantiate(enemyType5, enemyDrop, Quaternion.identity);
+            }
             //Debug.Log("Spawning new enemy");
-            GameObject newEnemy = Instantiate(enemyPrefab, enemyDrop, Quaternion.identity);
+
             //GameState.EnemyCount++;
         }
     }
