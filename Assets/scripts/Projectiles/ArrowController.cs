9f35b634f97399684c70ca9086095c6e8301fe5f
Andrew Nguyen <anguyen1281@student.cccd.edu>
2022-12-04 16:17:06 -0800

    fixed arrows

---
+++ Assets/scripts/Projectiles/ArrowController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    public GameObject arrowRef;
+    public Transform ArrowLauncher;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Coroutine startSpawn = StartCoroutine(ArrowRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    IEnumerator ArrowRoutine()
+    {
+        while(true)
+        {
+            Instantiate(arrowRef, ArrowLauncher.position, ArrowLauncher.rotation);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
